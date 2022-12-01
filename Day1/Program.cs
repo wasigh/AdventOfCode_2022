f092da3931de59cd41c3735d9f93bee5bbac902d
Hans van Leuken <hansvanleuken@gmail.com>
2022-12-01 09:49:14 +0100

    Day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var sTestFile = "test.in";
+var sLines = File.ReadAllLines(sTestFile);
+
+var nCurrent = 0;
+var nMax = 0;
+var lDwrfs = new List<int>();
+
+
+foreach (var sLine in sLines)
+{
+    if (string.IsNullOrEmpty(sLine))
+    {
+        lDwrfs.Add(nCurrent);
+        nCurrent= 0;
+    }
+    else
+    {
+        var nVal = int.Parse(sLine);
+        nCurrent  += nVal;
+        nMax = Math.Max(nCurrent, nMax);
+    }
+}
+
+Console.WriteLine("max total: " + nMax);
+
+// Sort 
+lDwrfs.Sort();
+lDwrfs.Reverse();
+var nTotal = lDwrfs.Take(3).Sum();
+Console.WriteLine("Top 3: " + nTotal);
+
+
