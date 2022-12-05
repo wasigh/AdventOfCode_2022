79e7961f666a80fef6707cb2976ab53e5dcaba0b
Hans van Leuken <hansvanleuken@gmail.com>
2022-12-05 09:26:09 +0100

    Day3 + Day4

---
+++ Day3/Program.cs
@@ -0,0 +1,65 @@
+﻿
+var sLines = File.ReadAllLines("data.in");
+var nTotal = 0;
+
+// foreach (var sLine in sLines)
+// {
+//     //Console.Out.WriteLine(sLine);
+//     var sIn = sLine.Trim();
+//     var nLength = sIn.Length;
+//     var nHalfLength = nLength / 2;
+//     var sDict = new Dictionary<char, int>();
+
+//     var sFirstPart = sIn.Substring(0, nHalfLength);
+
+//     for (var i = nHalfLength; i < nLength; i++)
+//     {
+//         var c1 = sIn[i];
+//         if (sFirstPart.Contains(c1))
+//         {
+//             Console.Out.WriteLine(c1);
+
+//             if ((c1 - 'A') <= 26)
+//                 nTotal += (c1 - 'A') + 27;
+
+//             else
+//                 nTotal += (c1 - 'a') + 1;
+
+//             //Console.Out.WriteLine(nTotal);  
+//             break;
+//         }
+
+//     }
+// }
+
+
+
+// part2
+for (var n = 0; n < sLines.Length; n += 3)
+{
+    var sL1 = sLines[n];
+    var sL2 = sLines[n + 1];
+    var sL3 = sLines[n + 2];
+
+    foreach (var c in sL1)
+    {
+        if (sL2.Contains(c) && sL3.Contains(c))
+        {
+            Console.WriteLine("Found:" + c);
+            if ((c - 'A') <= 26)
+                nTotal += (c - 'A') + 27;
+
+            else
+                nTotal += (c - 'a') + 1;
+
+            break;
+        }
+    }
+
+}
+
+Console.Out.WriteLine(nTotal);
+
+
+
+
---
+++ Day4/Program.cs
@@ -0,0 +1,55 @@
+﻿
+
+
+var sLines = File.ReadAllLines("data.in");
+var nCount = 0;
+
+foreach(var sLine in sLines)
+{
+    var lineParts = sLine.Trim().Split(",");
+
+    var p1 = new Linepart(lineParts[0]);
+    var p2 = new Linepart(lineParts[1]);
+
+    if (p1.OverLaps(p2) || p2.OverLaps(p1) )
+    {
+        Console.Out.WriteLine(sLine);
+        nCount ++;
+    }
+
+}
+
+
+Console.Out.WriteLine(nCount);
+
+
+
+class Linepart
+{
+
+    int nMin = 0;
+    int nMax = 0;
+     
+
+    public Linepart(string sIn)
+    {
+        var sParts = sIn.Split("-");
+        this.nMin = int.Parse(sParts[0]);
+        this.nMax = int.Parse(sParts[1]);
+    }
+
+    public bool Contains(Linepart other)
+    {
+        return (other.nMin >= this.nMin && other.nMax <= this.nMax);
+    }
+
+    public bool OverLaps(Linepart other)
+    {
+        if (other.nMin >= this.nMin && other.nMin <= this.nMax)
+            return true;
+        if (other.nMax >= this.nMin && other.nMax <= this.nMax)
+            return true;
+        return false;
+
+    }
+}
